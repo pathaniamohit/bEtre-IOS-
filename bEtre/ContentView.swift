f27c82e36e0e267a4595f8b41d97e2fcc66f6871
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-09-23 16:52:04 -0400

    Adding Project Files

---
+++ bEtre/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bEtre
+//
+//  Created by Mohit Pathania on 2024-09-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
