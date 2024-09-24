6fcac52fdeb35afba6b5fc4b2aa436b09c0a19d9
Your Github Username <your-singhamritpal1919@gmail.com>
2024-09-24 13:36:33 -0400

    Login View

---
+++ bEtre/Authentication/SessionManager.swift
@@ -0,0 +1,8 @@
+//
+//  SessionManager.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-09-24.
+//
+
+import Foundation
---
+++ bEtre/Authentication/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-09-24.
+//
+
+import Foundation
