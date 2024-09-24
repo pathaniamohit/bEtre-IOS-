a6309e6f3c35e002b89434518abe2af8ff0d62b1
Your Github Username <your-singhamritpal1919@gmail.com>
2024-09-24 12:08:52 -0400

    First Commit

---
+++ bEtre/bEtreApp.swift
@@ -0,0 +1,17 @@
+//
+//  bEtreApp.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-09-24.
+//
+
+import SwiftUI
+
+@main
+struct bEtreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
