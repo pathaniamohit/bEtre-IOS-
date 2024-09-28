ae535e469be5c3e928c6ba1dda9857299ad6bc40
Your Github Username <your-singhamritpal1919@gmail.com>
2024-09-27 21:00:51 -0400

    bEtre

---
+++ bEtre/Authentication/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-09-27.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
---
+++ bEtre/Authentication/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-09-27.
+//
+
+import Foundation
