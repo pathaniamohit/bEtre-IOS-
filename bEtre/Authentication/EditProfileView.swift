6584ea7ca5e49c8dae17624455de0e65b2ac48eb
Your Github Username <your-singhamritpal1919@gmail.com>
2024-10-03 21:40:35 -0400

    Splash Screen

---
+++ bEtre/Authentication/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-10-03.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
---
+++ bEtre/Authentication/SplashScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreenView.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-10-03.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
