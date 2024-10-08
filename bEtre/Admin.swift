61ac77040c81d552d6864dc8731e199d1f1ccc0e
pathaniamohit <pathaniamohit0711raj@gmail.com>
2024-10-07 23:17:24 -0400

    project

--- bEtre/Admin.swift
+++ bEtre/Admin.swift
@@ -5,4 +5,20 @@
 //  Created by Mohit Pathania on 2024-10-07.
 //
 
-import Foundation
+import SwiftUI
+
+struct AdminView: View {
+    var body: some View {
+        Text("Welcome to the Admin Panel")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+            .navigationTitle("Admin Dashboard")
+    }
+}
+
+struct AdminView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdminView()
+    }
+}
