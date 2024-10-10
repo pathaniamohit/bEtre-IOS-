ea8ecbc86798fb87cd43a0bc660b425c96c5f661
singhamritpal1919 <130592524+singhamritpal1919@users.noreply.github.com>
2024-10-10 17:30:33 -0400

    create view updated

---
+++ bEtre/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  bEtre
+//
+//  Created by Amritpal Gill on 2024-10-10.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
